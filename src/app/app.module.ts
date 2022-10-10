554fbff2f39dab8d8a7ad3243492d0f8add502e1
HongHiep75 <101515554+HongHiep75@users.noreply.github.com>
2022-10-10 22:30:36 +0700

    update user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@
  */
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
@@ -21,6 +20,10 @@ import {
   NbToastrModule,
   NbWindowModule,
 } from '@nebular/theme';
+import {NgModule} from "@angular/core";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 const configToast: any = {
   timeOut: 3000,
@@ -50,6 +53,7 @@ const configToast: any = {
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
     ToastrModule.forRoot(configToast),
+    NgbModule,
   ],
   bootstrap: [AppComponent],
 })
